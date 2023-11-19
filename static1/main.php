cf4f5c8a517ad3f482f20ae69ee9ebb335ed9bb2
もっちゃん <s7056i3056@gmail.com>
2023-11-19 18:23:49 +0900

    feat: 静的メソッドの実装例

---
+++ static1/main.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1); ?>
+<body>
+    <?php
+        require_once dirname(__FILE__) . '/ExcelColumnConverter.php';
+        echo ExcelColumnConverter::calcAlphabetColumnName(3), PHP_EOL;
+        echo ExcelColumnConverter::calcNumberColumnName('F'), PHP_EOL;
+    ?>
+</body>
